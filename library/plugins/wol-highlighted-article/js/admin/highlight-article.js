fa7c422cafd97664f3ba1bda7132d43df382fa25
flickerfate <flickerfate@gmail.com>
2017-12-21 04:45:21 +0300

    add info about cap of post title length

--- library/plugins/wol-highlighted-article/js/admin/highlight-article.js
+++ library/plugins/wol-highlighted-article/js/admin/highlight-article.js
@@ -11,4 +11,27 @@
         },
         palettes: ['#000', '#F9F5F9', '#F7FBFF', '#A1B0C3', '#F8F9F5', '#F5FFFB']
     });
+
+    $('#post [name="post_title"]').change(function () {
+        calcPostTitleLength($(this));
+    });
+    $('#post [name="post_title"]').keyup(function () {
+        calcPostTitleLength($(this));
+    });
+
+    $(function () {
+        $('#post [name="post_title"]').before($('<div id="post_title_length" style="color:#ff3f00"></div>'));
+        calcPostTitleLength($('#post [name="post_title"]'));
+    });
+
+    function calcPostTitleLength($title) {
+        var l = $title.val().length;
+        var max = 54;
+        if (max-l > 0) {
+            $('#post_title_length').html('Left ' + (max-l) + ' characters otherwise the template may be broken.');
+        } else {
+            $('#post_title_length').html('Attention! The template may be broken. Title length more than ' + max + ' characters.');
+        }
+        $('#title-prompt-text').css('top','18px');
+    }
 })(jQuery);
