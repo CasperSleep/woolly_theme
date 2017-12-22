95dfcf5820851cf0d844e6ca95bc966f151faf3a
flickerfate <flickerfate@gmail.com>
2017-12-22 14:53:43 +0300

    add Outbrain code

--- header.php
+++ header.php
@@ -73,6 +73,14 @@ $theme_url = get_template_directory_uri();
 
         return t;
     }(document, "script", "twitter-wjs"));</script>
+<script data-obct type="text/javascript">
+    /** DO NOT MODIFY THIS CODE**/
+    !function(_window, _document) {
+        var OB_ADV_ID='00b65ba00afd1beab704faa1b041194a9e';
+        if (_window.obApi) {var toArray = function(object) {return Object.prototype.toString.call(object) === '[object Array]' ? object : [object];};_window.obApi.marketerId = toArray(_window.obApi.marketerId).concat(toArray(OB_ADV_ID));return;}
+        var api = _window.obApi = function() {api.dispatch ? api.dispatch.apply(api, arguments) : api.queue.push(arguments);};api.version = '1.1';api.loaded = true;api.marketerId = OB_ADV_ID;api.queue = [];var tag = _document.createElement('script');tag.async = true;tag.src = '//amplify.outbrain.com/cp/obtp.js';tag.type = 'text/javascript';var script = _document.getElementsByTagName('script')[0];script.parentNode.insertBefore(tag, script);}(window, document);
+    obApi('track', 'PAGE_VIEW');
+</script>
 <?php $home_url = home_url(); ?>
 <div id="page" class="site">
 	<div class="site-inner">
